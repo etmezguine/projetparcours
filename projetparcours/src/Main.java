1dc95f5d542e6f3821b3d7fd97b6434740386010
Alex <etmezguine@eisti.eu>
2014-06-10 08:44:48 +0200

    test fetch larti

--- projetparcours/src/Main.java
+++ projetparcours/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Coucou toi");
+		System.out.println("Coucou toi, essaye de fetch !");
 		System.out.println("Oui c'est moi !");
 	}
 
