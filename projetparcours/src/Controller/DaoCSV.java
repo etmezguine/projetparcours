6a715dab20d129d6f6a910b82af9fed12d7d1833
Alex <etmezguine@eisti.eu>
2014-06-10 13:47:44 +0200

    map ok

--- projetparcours/src/Controller/DaoCSV.java
+++ projetparcours/src/Controller/DaoCSV.java
@@ -14,13 +14,12 @@ public class DaoCSV {
 	 private final static String DATA_FILE_NAME = "eleves.csv";
 
 	 public List<Integer> findData() throws IOException {
+		 
 		 File file = new File(RESOURCES_PATH + DATA_FILE_NAME);
 		 FileReader fr = new FileReader(file);
 		 CSVReader reader = new CSVReader(new FileReader("yourfile.csv"),SEPARATOR);
 		 return null;
-					
-					
-		
+		 
 		}
 	    
 }
--- projetparcours/src/Main.java
+++ projetparcours/src/Main.java
@@ -1,3 +1,9 @@
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 public class Main{
 
@@ -7,7 +13,15 @@ public class Main{
 	public static void main(String[] args) {
 		System.out.println("Coucou toi, essaye de fetch !");
 		System.out.println("Oui c'est moi, le bg et oui !!");
-	
+		
+		URLConnection con = new URL("http://maps...").openConnection();
+		InputStream is = con.getInputStream();
+		byte bytes[] = new byte[con.getContentLength()];
+		is.read(bytes);
+		is.close();
+		Toolkit tk = getToolKit();
+		Image map = tk.createImage(bytes);
+		tk.prepareImage(map, -1, -1, null);
 	}
 
 }
