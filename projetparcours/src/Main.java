568f64e8d96f585ac0e42690e5ba8b74f2c9c527
lartiguebe <lartiguebe@eisti.eu>
2014-06-10 09:59:02 +0200

    Coucou toi

--- projetparcours/src/Main.java
+++ projetparcours/src/Main.java
@@ -1,12 +1,12 @@
 
-public class Main {
+public class Main{
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("Coucou toi, essaye de fetch !");
-		System.out.println("Oui c'est moi, le bg !");
+		System.out.println("Oui c'est moi, le bg et oui !!");
 	}
 
 }
