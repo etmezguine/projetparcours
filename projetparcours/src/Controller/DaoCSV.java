a39bc4f702a10587f9cc470d307183de3e1a31f6
Alex <etmezguine@eisti.eu>
2014-06-10 10:44:29 +0200

    CSV OP !

--- projetparcours/src/Controller/DaoCSV.java
+++ projetparcours/src/Controller/DaoCSV.java
@@ -3,6 +3,7 @@ package Controller;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 public class DaoCSV {
@@ -12,10 +13,10 @@ public class DaoCSV {
 	 private final static String RESOURCES_PATH = "src/main/resources/";
 	 private final static String DATA_FILE_NAME = "eleves.csv";
 
-	 public List<Integer> findData() throws FileNotFoundException {
+	 public List<Integer> findData() throws IOException {
 		 File file = new File(RESOURCES_PATH + DATA_FILE_NAME);
 		 FileReader fr = new FileReader(file);
-		 CSVReader reader = new CSVReader(new FileReader("yourfile.csv"));
+		 CSVReader reader = new CSVReader(new FileReader("yourfile.csv"),SEPARATOR);
 		 return null;
 					
 					
