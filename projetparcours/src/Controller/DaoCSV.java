9e7ba04a9feaddcace8abd69eec2ad65ebd78af7
Alex <etmezguine@eisti.eu>
2014-06-10 09:47:36 +0200

    ajout data access object

---
+++ projetparcours/src/Controller/DaoCSV.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+public class DaoCSV {
+	
+
+	 private final static char SEPARATOR = ',';
+	 private final static String RESOURCES_PATH = "src/main/resources/";
+	  private final static String DATA_FILE_NAME = "eleves.csv";
+
+			    public List<Integer> findData() throws FileNotFoundException {
+			        File file = new File(RESOURCES_PATH + DATA_FILE_NAME);
+			        FileReader fr = new FileReader(file);
+					return null;
+		
+		}
+	    
+}
